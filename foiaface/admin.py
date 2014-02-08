1ee23ab2421e2ccba5da3453860bfab01dd59c0e
jroo <ruihley@gmail.com>
2014-02-08 14:31:44 -0500

    added agency model

--- foiaface/admin.py
+++ foiaface/admin.py
@@ -4,3 +4,4 @@ from foiaface.models import Jurisdiction, Contact, Request
 admin.site.register(Jurisdiction)
 admin.site.register(Contact)
 admin.site.register(Request)
+admin.site.register(Agency)
--- foiaface/models.py
+++ foiaface/models.py
@@ -1,30 +1,38 @@
 from django.db import models
 
 class Jurisdiction(models.Model):
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=255)
     contact = models.ForeignKey("Contact")
-    parent_jurisdition = models.ForeignKey("Jurisdiction", null=True, blank=True)
+    parent_jurisdition = models.ForeignKey("Jurisdiction", null=True,
+        blank=True)
 
     def __str__(self):
         return self.name
 
+
+class Agency(models.Model):
+    name = models.CharField(max_length=255)
+    contact = models.ForeignKey("Contact")
+
+
 class Contact(models.Model):
 
-    first_name = models.CharField(max_length=255, null=False)
-    last_name = models.CharField(max_length=255, null=False)
-    email = models.EmailField(null=False)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
 
 
 class Request(models.Model):
-    requester_first_name = models.CharField(max_length=255, null=False)
-    requester_last_name = models.CharField(max_length=255, null=False)
-    requester_email = models.EmailField(max_length=255, null=False)
-    text = models.TextField(null=False)
+    requester_first_name = models.CharField(max_length=255)
+    requester_last_name = models.CharField(max_length=255)
+    requester_email = models.EmailField(max_length=255)
+    text = models.TextField()
     jurisdiction = models.ManyToManyField("Jurisdiction")
     created_at = models.DateTimeField()
 
     def __str__(self):
-        return "Request by %s %s to %s on %s" % (requester_first_name, requester_last_name, jurisdiction, created_at)
+        return "Request by %s %s to %s on %s" % (requester_first_name,
+            requester_last_name, jurisdiction, created_at)
