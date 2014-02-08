5b892cd7c29c214fc633a0584146a8aad5ad4260
jroo <ruihley@gmail.com>
2014-02-08 13:22:13 -0500

    fixed model bugs, added model for basic request

--- foiaface/models.py
+++ foiaface/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 class Jurisdiction(models.Model):
-	name = models.CharField(max_length=255)
-	contact = models.ForeignKey(Contact)
-	parent_jurisdition = models.ForeignKey(Jurisdiction)
+	name = models.CharField(max_length=255, null=False)
+	contact = models.ForeignKey("Contact")
+	parent_jurisdition = models.ForeignKey("Jurisdiction")
 
 
 class Contact(models.Model):
-	first_name = models.CharField(max_length=255)
-	last_name = models.CharField(max_length=255)
-	email = models.EmailField(required=True)
+	first_name = models.CharField(max_length=255, null=False)
+	last_name = models.CharField(max_length=255, null=False)
+	email = models.EmailField(null=False)
+
+
+class Request(models.Model):
+	requester_first_name = models.CharField(max_length=255, null=False)
+	requester_last_name = models.CharField(max_length=255, null=False)
+	requester_email = models.EmailField(max_length=255, null=False)
+	text = models.TextField(null=False)
+	jurisdiction = models.ManyToManyField("Jurisdiction")
