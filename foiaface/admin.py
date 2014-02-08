617818220e1cbfab612bf12c09548633c7158540
jroo <ruihley@gmail.com>
2014-02-08 13:39:35 -0500

    installed foiaface app, added admin for new models

---
+++ foiaface/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from foiaface.models import Jurisdiction, Contact, Request
+
+admin.site.register(Jurisdiction)
+admin.site.register(Contact)
+admin.site.register(Request)
+
